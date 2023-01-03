4a7c0f7e83cfc3e70605c469e287db61b91680a0
krishna <brkkrishna9@gmail.com>
2023-01-03 21:57:18 +0530

    added commands

--- 03-variable.sh
+++ 03-variable.sh
@@ -7,3 +7,12 @@ Student=KK
 echo Trainer name is $Trainer
 echo Class is ${Class}
 echo STUDENT name is $Student
+
+# Date syntax
+
+Date=$(date +%F)
+echo date is $Date
+
+#Arthematic syntax
+ADD=$((2+3))
+echo result - $ADD
