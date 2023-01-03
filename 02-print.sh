49b2058c6f8c00055091cf00bdbe6924c6b37e83
krishna <brkkrishna9@gmail.com>
2023-01-03 21:40:39 +0530

    added commands

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,19 @@
 echo Hello world
 
 echo "*** Hello Welcome ***"
+
+# color code syntax: echo -e "\e[ColmMESSAGE\[0m"
+# RED -31- echo -e "\e[31mMESSAGE\[0m"
+# GREEN -32- echo -e "\e[32mMESSAGE\[0m"
+# YELLOW -33- echo -e "\e[33mMESSAGE\[0m"
+# BLUE -34- echo -e "\e[34mMESSAGE\[0m"
+# NAGENTA -35- echo -e "\e[35mMESSAGE\[0m"
+# CYAN -36- echo -e "\e[36mMESSAGE\[0m"
+
+echo -e "\e[31mMESSAGE\[0m"
+echo -e "\e[32mMESSAGE\[0m"
+echo -e "\e[33mMESSAGE\[0m"
+echo -e "\e[34mMESSAGE\[0m"
+echo -e "\e[35mMESSAGE\[0m"
+echo -e "\e[36mMESSAGE\[0m"
+
