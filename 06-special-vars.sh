0ff14b3f0ea8aca7b35401986ea6c6ef46778df2
krishna <brkkrishna9@gmail.com>
2023-01-09 16:11:44 +0530

    new scripts

---
+++ 06-special-vars.sh
@@ -0,0 +1,20 @@
+echo "special vars from script"
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+
+example()
+{
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+example xyz 10
+
