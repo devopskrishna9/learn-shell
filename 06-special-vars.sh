cc52fc2d6c8c16224afe73d2c103d26e22278f1e
krishna <brkkrishna9@gmail.com>
2023-01-09 16:23:05 +0530

    new scripts

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,3 +1,15 @@
+example()
+{
+  echo "following values from function"
+  echo "0 - $0"  # not application in function it will print script name
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+
 echo "special vars from script"
 echo "0 - $0"
 echo "1 - $1"
@@ -6,15 +18,8 @@ echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
 
-example()
-{
-  echo "0 - $0"
-  echo "1 - $1"
-  echo "2 - $2"
-  echo "* - $*"
-  echo "@ - $@"
-  echo "# - $#"
-}
 
+example $* #inputs we are passing
 example xyz 10
+
 
